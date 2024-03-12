ff1f9b5737b3c98afffbfe6bdd9a3c3285f2ac64
jzimme14 <jakobzimmermann@outlook.com>
2024-03-12 12:37:38 +0100

    !

--- lolin32_transponder_pio/src/main.cpp
+++ lolin32_transponder_pio/src/main.cpp
@@ -3,11 +3,10 @@
 #include <HTTPClient.h>
 #include <string.h>
 
-// letzte version meines transponders mit bme280 und esp und einem http-server auf meinem beelink
+#include <Wire.h>
+#include <Adafruit_BME280.h>
 
-void deepSleep();
-void sendHttpRequest();
-void sendHttpPostRequest();
+// letzte version meines transponders mit bme280 und esp und einem http-server auf meinem beelink
 
 typedef struct
 {
@@ -17,6 +16,17 @@ typedef struct
   float pressure;
 } dataframe;
 
+void sendDataToServer(String measDataString);
+void initBME280();
+dataframe getBME280Readings();
+void deepSleep();
+String dataframeToString(dataframe d);
+
+void sendHttpGetRequest();
+
+/* !!!! id is set here. It is unique and has to be set here before flashing new sensorunit !!!! */
+const int id = 1;
+
 // wifi data
 const char *ssid = "TP-Link_4D9A";
 const char *password = "78811928";
@@ -25,13 +35,26 @@ const char *password = "78811928";
 const char *server = "192.168.1.119"; // ip of butzdigga-server
 const int port = 8080;
 const char *filename = "/index.html";
-
 const char *url = "192.168.1.119:8080/index.html";
+
+// BME stuff
+Adafruit_BME280 bme;
 
 void setup()
 {
   Serial.begin(9600);
+  while (!Serial)
+    ;
+  initBME280();
+}
 
+void loop()
+{
+  dataframe measData = {id, 0, 0, 0};
+  measData = getBME280Readings();
+  delay(10);
+
+  /* Connect to Wifi */
   WiFi.begin(ssid, password);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -39,25 +62,37 @@ void setup()
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nConnected!");
-  Serial.print("local IP: ");
+  Serial.print("\nlocal IP: ");
   Serial.println(WiFi.localIP());
   Serial.print("gateway IP: ");
   Serial.println(WiFi.gatewayIP());
 
-  sendHttpRequest();
-  delay(2000);
-  sendHttpPostRequest();
+  Serial.println(dataframeToString(measData));
+
+  sendDataToServer(dataframeToString(measData));
+
+  delay(10);
+  WiFi.disconnect();
+  deepSleep();
 }
 
-void loop()
+void initBME280()
 {
+  Wire.begin(23, 19);
+  Adafruit_BME280 b;
+  int status = b.begin(0x76, &Wire);
+  bme = *(&b);
+  delay(10);
 }
 
-void sendHttpPostRequest()
+dataframe getBME280Readings()
 {
-  // Daten, die an den Server gesendet werden sollen
-  String postData = "hello";
+  return {id, bme.readTemperature(), bme.readHumidity(), bme.readPressure()};
+}
+
+/* Builds a POST Request and sends it to the server */
+void sendDataToServer(String measDataString)
+{
 
   // Verbindung zum Server herstellen
   HTTPClient http;
@@ -65,7 +100,7 @@ void sendHttpPostRequest()
   http.addHeader("Content-Type", "text/plain");
 
   // HTTP POST-Anfrage senden
-  int httpResponseCode = http.POST(postData);
+  int httpResponseCode = http.POST(measDataString);
 
   // Antwort des Servers ausgeben
   if (httpResponseCode > 0)
@@ -85,7 +120,12 @@ void sendHttpPostRequest()
   http.end();
 }
 
-void sendHttpRequest()
+String dataframeToString(dataframe d)
+{
+  return (String(d.transponderID, 2) + "," + String(d.temp, 2) + "," + String(d.hum, 2) + "," + String(d.pressure, 2));
+}
+
+void sendHttpGetRequest()
 {
   // Verbindung zum Server herstellen
   WiFiClient client;
@@ -117,6 +157,6 @@ void sendHttpRequest()
 
 void deepSleep()
 {
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_32, HIGH);
+  esp_sleep_enable_timer_wakeup(10000000); // * 1e-6 eg microseconds
   esp_deep_sleep_start();
 }
